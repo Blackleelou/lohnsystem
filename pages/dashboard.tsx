c807335e0027ccad2d473e8e79c99695607344fc
Blackleelou <jantzen.chris@gmail.com>
2025-05-15 18:40:46 +0200

    Add files via upload

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,1 @@
-import Layout from '@/components/Layout';
-
-export default function Dashboard() {
-  return (
-    <Layout>
-      <h1>Dashboard</h1>
-      <p>Hier kommt deine Lohnübersicht hin.</p>
-    </Layout>
-  );
-}
+...
