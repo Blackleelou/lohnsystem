02073dfed59a4f2757907805e04ef1e643ee52f2
Blackleelou <jantzen.chris@gmail.com>
2025-05-15 20:32:54 +0200

    Add files via upload

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -7,15 +7,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { email, password } = req.body;
 
   if (email === 'test@user.de' && password === '12345678') {
-    res.setHeader('Set-Cookie', serialize('userId', 'testuser', {
-      path: '/',
-      httpOnly: true,
-      sameSite: 'lax',
-      secure: true,
+    const cookie = serialize("userId", "testuser", {
+      path: "/",
+      httpOnly: false,
+      sameSite: "lax",
+      secure: process.env.NODE_ENV === "production",
       maxAge: 60 * 60 * 24,
-    }));
+    });
+
+    res.setHeader("Set-Cookie", cookie);
     return res.status(200).end();
   }
 
-  return res.status(401).send('Login fehlgeschlagen');
+  return res.status(401).send("Login fehlgeschlagen");
 }
