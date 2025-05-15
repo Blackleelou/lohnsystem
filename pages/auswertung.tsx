54d67d58124d9bcdd0e614eb06628336ae074c6d
Blackleelou <jantzen.chris@gmail.com>
2025-05-15 17:57:25 +0200

    Add files via upload

---
+++ pages/auswertung.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+
+export default function AuswertungPage() {
+  return (
+    <Layout>
+      <h1>Auswertungen</h1>
+      <p>Hier findest du deine monatlichen Lohn- und Schichtauswertungen.</p>
+    </Layout>
+  );
+}
