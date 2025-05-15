6fc1f20f7f595bc3659d6eed339065ac19e69396
Blackleelou <jantzen.chris@gmail.com>
2025-05-15 13:19:47 +0200

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,1 +1,7 @@
+import { parse } from 'cookie';
+import type { NextApiRequest } from 'next';
 
+export function getSessionUser(req: NextApiRequest) {
+  const cookies = req.headers.cookie ? parse(req.headers.cookie) : {};
+  return cookies.userId || null;
+}
