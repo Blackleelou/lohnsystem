f99a45aa6532d3da28e571c31b44a1aff5dc5739
Blackleelou <jantzen.chris@gmail.com>
2025-05-15 20:32:23 +0200

    Add files via upload

--- pages/api/auth/status.ts
+++ pages/api/auth/status.ts
@@ -4,5 +4,5 @@ import { parse } from 'cookie';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const cookies = req.headers.cookie ? parse(req.headers.cookie) : {};
   const loggedIn = !!cookies.userId;
-  res.status(200).json({ loggedIn });
+  res.status(200).json({ loggedIn, cookie: cookies.userId || null });
 }
