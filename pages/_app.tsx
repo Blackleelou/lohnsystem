291d0fec306d24a66c81282713fd6f1ecdd334a0
Blackleelou <jantzen.chris@gmail.com>
2025-05-16 12:17:03 +0200

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,1 +1,11 @@
-// Dummy-Inhalt für pages/_app.tsx
+import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
+import "@/styles/globals.css";
+
+export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+}
