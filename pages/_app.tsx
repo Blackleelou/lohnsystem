5964e10dd62c16d9752e9b830b760b0c084e1b6e
Blackleelou <jantzen.chris@gmail.com>
2025-05-16 12:23:57 +0200

    Add files via upload

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import "@/styles/globals.css";
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  const getLayout = (Component as any).getLayout || ((page: React.ReactNode) => page);
+
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {getLayout(<Component {...pageProps} />)}
     </SessionProvider>
   );
 }
