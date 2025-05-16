d924cd3662f3e3466c33744ab6291c5c9d4f28d0
Blackleelou <jantzen.chris@gmail.com>
2025-05-16 12:53:40 +0200

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,4 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-
-  // Beispiel: Setze Cookie "visited=true"
-  response.cookies.set("visited", "true", { path: "/" });
-
-  return response;
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard", "/profile", "/settings", "/auswertung"],
