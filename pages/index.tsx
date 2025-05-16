6b5a1a3f371b718efc902564621a21dfede6a369
Blackleelou <jantzen.chris@gmail.com>
2025-05-16 14:08:31 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,19 @@
-export async function getServerSideProps() {
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+import { GetServerSidePropsContext } from "next";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      },
+    };
+  }
+
   return {
     redirect: {
       destination: '/login',
