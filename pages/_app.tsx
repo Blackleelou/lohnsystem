fd81064d1e0c385cdbdd31090f8307568effc54e
Blackleelou <jantzen.chris@gmail.com>
2025-05-21 07:58:34 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
+// Datei: pages/_app.tsx
+
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import CookieBanner from "@/components/CookieBanner";
-import "@/styles/globals.css";
+import "@/styles/globals.css"; // Tailwind-Stile aktivieren
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const getLayout = (Component as any).getLayout || ((page: React.ReactNode) => page);
