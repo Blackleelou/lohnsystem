2c13346b5055a964a9abfce535acd5e2d5687d28
Blackleelou <jantzen.chris@gmail.com>
2025-05-22 11:15:35 +0200

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,10 +3,15 @@ import UserMenu from "./UserMenu";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
-      <header className="bg-white shadow px-6 py-3 flex justify-end sticky top-0 z-40">
+      {/* Feste obere Leiste mit Benutzer-Menü */}
+      <header className="bg-white shadow px-6 py-3 flex justify-end items-center sticky top-0 z-50">
         <UserMenu />
       </header>
-      <main className="flex-1 p-6">{children}</main>
+
+      {/* Hauptinhalt */}
+      <main className="flex-1 px-4 py-6">
+        {children}
+      </main>
     </div>
   );
 }
