74153fed6973b0ca31cb7d064d1e50da3bf47db8
Blackleelou <jantzen.chris@gmail.com>
2025-05-23 19:00:47 +0200

    Create constants.ts

---
+++ components/Board/constants.ts
@@ -0,0 +1,18 @@
+// components/Board/constants.ts
+
+export const STATUS_OPTIONS = [
+  "geplant",
+  "offen",
+  "in Bearbeitung",
+  "fertig/getestet", // zusammengeführt
+];
+
+export const CATEGORY_OPTIONS = [
+  "IT",
+  "Personal",
+  "Finanzen",
+  "Organisation",
+  "Kommunikation",
+  "Projekte",
+  "Sonstiges",
+];
