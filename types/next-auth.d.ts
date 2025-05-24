88c412f8e652a51e992eade468130d853ce8048d
Blackleelou <jantzen.chris@gmail.com>
2025-05-24 13:47:30 +0200

    Add files via upload

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,30 +1,13 @@
 import NextAuth from "next-auth";
-import { JWT } from "next-auth/jwt";
 
 declare module "next-auth" {
   interface Session {
     user: {
       id: string;
       email: string;
-      username?: string | null;
       name?: string | null;
       image?: string | null;
+      mode?: "solo" | "company";
     };
-  }
-
-  interface User {
-    id: string;
-    email: string;
-    username?: string | null;
-    name?: string | null;
-    image?: string | null;
-  }
-}
-
-declare module "next-auth/jwt" {
-  interface JWT {
-    id: string;
-    email: string;
-    username?: string | null;
   }
 }
