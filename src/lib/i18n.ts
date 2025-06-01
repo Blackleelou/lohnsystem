aa39d33c9e8e7de892646ac9b94c7a3063a7bf92
Blackleelou <jantzen.chris@gmail.com>
2025-06-01 22:07:20 +0200

    Update i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,8 +1,8 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationDE from '../public/locales/de/translation.json';
-import translationEN from '../public/locales/en/translation.json';
+import translationDE from "@/locales/de/translation.json";
+import translationEN from "@/locales/en/translation.json";
 
 i18n
   .use(initReactI18next)
