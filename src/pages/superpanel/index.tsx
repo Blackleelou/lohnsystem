40488618db0d044d4e21afcbaa5b2d1687d9c9d5
Blackleelou <jantzen.chris@gmail.com>
2025-06-02 07:17:34 +0200

    Delete src/pages/superpanel/index.tsx

--- src/pages/superpanel/index.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const Superpanel = dynamic(() => import("@/components/Superpanel"), { ssr: false });
-
-export default function SuperpanelPage() {
-  return <Superpanel />;
-}
