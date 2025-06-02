4ce78c7ce39e76bf19bf3ef252337bf716f49f1d
Blackleelou <jantzen.chris@gmail.com>
2025-06-02 10:56:25 +0200

    Add files via upload

---
+++ src/pages/superadmin/board.tsx
@@ -0,0 +1,11 @@
+// src/pages/superadmin/board.tsx
+import BoardPage from "@/modules/board/BoardPage";
+import SuperadminLayout from "@/components/superadmin/SuperadminLayout";
+
+export default function SuperadminBoardPage() {
+  return (
+    <SuperadminLayout>
+      <BoardPage />
+    </SuperadminLayout>
+  );
+}
