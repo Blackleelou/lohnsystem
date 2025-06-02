bfef074abfe6e3cac8607fb27492b3c3ac04c327
Blackleelou <jantzen.chris@gmail.com>
2025-06-02 10:05:18 +0200

    Update board.tsx

--- src/pages/admin/board.tsx
+++ src/pages/admin/board.tsx
@@ -1,11 +1,11 @@
-// pages/admin/board.tsx
+// src/pages/admin/board.tsx
 import BoardPage from "@/modules/board/BoardPage";
 import SuperadminLayout from "@/components/superadmin/SuperadminLayout";
 
 export default function AdminBoardPage() {
-  return <BoardPage />;
+  return (
+    <SuperadminLayout>
+      <BoardPage />
+    </SuperadminLayout>
+  );
 }
-
-AdminBoardPage.getLayout = (page: React.ReactNode) => (
-  <SuperadminLayout>{page}</SuperadminLayout>
-);
