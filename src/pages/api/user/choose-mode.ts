0a8011d136b190ed98a2aa1a9080bad40c6c5fee
Blackleelou <jantzen.chris@gmail.com>
2025-06-03 10:28:58 +0200

    Create choose-mode.ts

---
+++ src/pages/api/user/choose-mode.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session?.user?.email) return res.status(401).end();
+
+  await prisma.user.update({
+    where: { email: session.user.email },
+    data: { hasChosenMode: true },
+  });
+
+  res.status(200).json({ ok: true });
+}
