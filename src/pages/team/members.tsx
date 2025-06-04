1d3023319ed3aa72e9053f0d3d21da03601b012a
Blackleelou <jantzen.chris@gmail.com>
2025-06-04 06:16:02 +0200

    Create members.tsx

---
+++ src/pages/team/members.tsx
@@ -0,0 +1,15 @@
+import Layout from "@/components/common/Layout";
+import TeamSidebar from "@/components/team/TeamSidebar";
+export default function TeamMembersPage() {
+  return (
+    <Layout>
+      <div className="flex max-w-4xl mx-auto py-12">
+        <TeamSidebar />
+        <main className="flex-1 pl-8">
+          <h1 className="text-2xl font-bold text-blue-700 mb-2">Mitglieder</h1>
+          <div className="text-gray-500">Hier kommt die Mitgliederverwaltung hin.</div>
+        </main>
+      </div>
+    </Layout>
+  );
+}
