34e3699b17057209c2b9ab9ce69bc0a0b3d642c2
Blackleelou <jantzen.chris@gmail.com>
2025-06-04 21:49:00 +0200

    Add files via upload

---
+++ src/pages/team/invites.tsx
@@ -0,0 +1,10 @@
+import TeamLayout from "@/components/team/TeamLayout";
+import TeamInviteGenerator from "@/components/team/TeamInviteGenerator";
+
+export default function InvitePage() {
+  return (
+    <TeamLayout>
+      <TeamInviteGenerator />
+    </TeamLayout>
+  );
+}
---
+++ src/pages/team/security.tsx
@@ -0,0 +1,10 @@
+import TeamLayout from "@/components/team/TeamLayout";
+import AccessCodePanel from "@/components/team/AccessCodePanel";
+
+export default function SecurityPage() {
+  return (
+    <TeamLayout>
+      <AccessCodePanel />
+    </TeamLayout>
+  );
+}
