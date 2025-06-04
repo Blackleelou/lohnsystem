0bf0c7be1e6b8ee97f64bfc8f1127b85eee3c1a3
Blackleelou <jantzen.chris@gmail.com>
2025-06-04 22:27:30 +0200

    Update create-access-code.ts

--- src/pages/api/team/create-access-code.ts
+++ src/pages/api/team/create-access-code.ts
@@ -20,7 +20,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       code,
       companyId: session.user.companyId,
       password: password || null,
-      expiresAt,
+      validUntil,
       role: role || "viewer",
     },
   });
