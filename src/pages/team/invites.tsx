c872942c7535eefe01f89627c4f446600a3b0f43
Blackleelou <jantzen.chris@gmail.com>
2025-06-05 11:36:06 +0200

    Update invites.tsx

--- src/pages/team/invites.tsx
+++ src/pages/team/invites.tsx
@@ -5,7 +5,10 @@ export default function TeamInvitePage() {
   return (
     <TeamLayout>
       <div className="max-w-6xl mx-auto mt-10 px-4">
-        <h1 className="text-2xl font-bold mb-6">Einladungen verwalten</h1>
+        {/* Nur eine einzige Überschrift */}
+        <h1 className="text-2xl font-bold mb-6">Teameinladungen verwalten</h1>
+
+        {/* Hier wird unsere Komponente eingefügt */}
         <TeamInviteGenerator />
       </div>
     </TeamLayout>
