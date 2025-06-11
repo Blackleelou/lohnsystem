ceb915671b791cdd0e6a9458b2c2d2d5b2d63dbe
Blackleelou <jantzen.chris@gmail.com>
2025-06-11 17:04:02 +0200

    Update update-invite-print.ts

--- src/pages/api/team/update-invite-print.ts
+++ src/pages/api/team/update-invite-print.ts
@@ -2,7 +2,7 @@
 
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../auth/[...nextauth]';
+import { authOptions } from '@/lib/authOptions'; // ✅ korrekt
 import prisma from '@/lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
