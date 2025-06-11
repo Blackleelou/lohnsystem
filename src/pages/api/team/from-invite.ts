db7575116c0dd59c43b26a5da06e144a168bf7fe
Blackleelou <jantzen.chris@gmail.com>
2025-06-11 23:45:59 +0200

    Update from-invite.ts

--- src/pages/api/team/from-invite.ts
+++ src/pages/api/team/from-invite.ts
@@ -1,7 +1,7 @@
 // src/pages/api/team/from-invite.ts
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import { prisma } from '@/lib/prisma'; // ggf. Pfad anpassen
+import { prisma } from '@/lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { token } = req.query;
