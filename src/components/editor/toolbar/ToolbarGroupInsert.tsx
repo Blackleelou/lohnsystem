eb684aa7d61da3c495534f8677ddbeb3eb11268d
Blackleelou <jantzen.chris@gmail.com>
2025-06-13 16:38:42 +0200

    Create ToolbarGroupInsert.tsx

---
+++ src/components/editor/toolbar/ToolbarGroupInsert.tsx
@@ -0,0 +1,9 @@
+// src/components/editor/toolbar/ToolbarGroupInsert.tsx
+
+export default function ToolbarGroupInsert() {
+  return (
+    <div className="text-sm text-gray-500 italic">
+      [Einfügen: Demnächst verfügbar]
+    </div>
+  );
+}
