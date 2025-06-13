d64a7a5ca9a97db8e93e24c651bc9e4b4bab9ceb
Blackleelou <jantzen.chris@gmail.com>
2025-06-13 15:15:15 +0200

    Create EditorCanvas.tsx

---
+++ src/components/editor/EditorCanvas.tsx
@@ -0,0 +1,11 @@
+import { Stage, Layer } from "react-konva";
+
+export default function EditorCanvas() {
+  return (
+    <Stage width={794} height={1123}>
+      <Layer>
+        {/* Elemente (Text, QR, Bilder) kommen später hier rein */}
+      </Layer>
+    </Stage>
+  );
+}
