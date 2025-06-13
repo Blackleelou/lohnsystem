d2e6e7fcaf70d6489b5e9d8ebc4c3877a5e8a850
Blackleelou <jantzen.chris@gmail.com>
2025-06-13 20:47:08 +0200

    Update EditorHeader.tsx

--- src/components/editor/EditorHeader.tsx
+++ src/components/editor/EditorHeader.tsx
@@ -1,11 +1,15 @@
-import EditorToolbarFormat from "./EditorToolbarFormat";
-import EditorToolbarText from "./EditorToolbarText";
+import ToolbarGroupFile from "./toolbar/ToolbarGroupFile";
+import ToolbarGroupFormat from "./toolbar/ToolbarGroupFormat";
+import ToolbarGroupText from "./toolbar/ToolbarGroupText";
+import ToolbarGroupInsert from "./toolbar/ToolbarGroupInsert";
 
 export default function EditorHeader() {
   return (
-    <div className="sticky top-0 z-30 w-full bg-white border-b shadow-sm px-4 py-2 flex flex-wrap items-center justify-start gap-4">
-      <EditorToolbarFormat />
-      <EditorToolbarText />
+    <div className="sticky top-0 z-30 w-full bg-white border-b shadow-sm px-4 py-2 flex flex-wrap items-center gap-6">
+      <ToolbarGroupFile />
+      <ToolbarGroupFormat />
+      <ToolbarGroupText />
+      <ToolbarGroupInsert />
     </div>
   );
 }
