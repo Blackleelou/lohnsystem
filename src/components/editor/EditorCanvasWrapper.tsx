ed42cf215f69b9850eb38dd5e98596e7b1c5cf2d
Blackleelou <jantzen.chris@gmail.com>
2025-06-13 17:20:56 +0200

    Update EditorCanvasWrapper.tsx

--- src/components/editor/EditorCanvasWrapper.tsx
+++ src/components/editor/EditorCanvasWrapper.tsx
@@ -6,6 +6,10 @@ import { useCanvasSize } from "./useCanvasSize";
 export default function EditorCanvasWrapper() {
   const { width, height } = useCanvasSize();
 
+  // Automatische Skalierung bei kleinen Bildschirmen
+  const maxWidth = typeof window !== "undefined" ? window.innerWidth : 375;
+  const scale = maxWidth < width + 40 ? maxWidth / (width + 40) : 1;
+
   return (
     <div className="overflow-auto w-full flex justify-center">
       <div
@@ -13,6 +17,8 @@ export default function EditorCanvasWrapper() {
         style={{
           width: `${width}px`,
           height: `${height}px`,
+          transform: `scale(${scale})`,
+          transformOrigin: "top center",
           margin: "2rem 0",
         }}
       >
