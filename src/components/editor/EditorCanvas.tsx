e1b2f93603afcaa018542b33575f31d9d3785f1f
Blackleelou <jantzen.chris@gmail.com>
2025-06-13 22:39:44 +0200

    Update EditorCanvas.tsx

--- src/components/editor/EditorCanvas.tsx
+++ src/components/editor/EditorCanvas.tsx
@@ -2,7 +2,7 @@ import { Stage, Layer, Text, Transformer, Image as KonvaImage } from "react-konv
 import { useEditorStore } from "./useEditorStore";
 import { useEffect, useRef, useState } from "react";
 import EditorToolbar from "./EditorToolbar";
-import useImage from "react-konva-utils/useImage";
+import useImage from "use-image";
 
 type Props = {
   width: number;
