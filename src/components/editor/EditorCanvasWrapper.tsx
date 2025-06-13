39b6b02730698fb06eceb1d96de3567e126638ab
Blackleelou <jantzen.chris@gmail.com>
2025-06-13 16:45:20 +0200

    Create EditorCanvasWrapper.tsx

---
+++ src/components/editor/EditorCanvasWrapper.tsx
@@ -0,0 +1,23 @@
+src/components/editor/EditorCanvasWrapper.tsx
+
+import { useCanvasSize } from "./useCanvasSize";
+import EditorCanvas from "./EditorCanvas";
+
+export default function EditorCanvasWrapper() {
+  const { width, height } = useCanvasSize();
+
+  return (
+    <div className="flex justify-center overflow-auto">
+      <div
+        style={{
+          width,
+          height,
+          background: "white",
+          boxShadow: "0 0 4px rgba(0,0,0,0.2)",
+        }}
+      >
+        <EditorCanvas />
+      </div>
+    </div>
+  );
+}
