ce69d006440f4ca00afbad5abb44af74e35bd8fc
Blackleelou <jantzen.chris@gmail.com>
2025-06-13 20:15:03 +0200

    Update EditorHeader.tsx

--- src/components/editor/EditorHeader.tsx
+++ src/components/editor/EditorHeader.tsx
@@ -3,7 +3,7 @@ import EditorToolbarText from "./EditorToolbarText";
 
 export default function EditorHeader() {
   return (
-    <div className="sticky top-0 z-30 w-full bg-white border-b shadow-sm px-4 py-2 flex flex-wrap items-center justify-between gap-4">
+    <div className="sticky top-0 z-30 w-full bg-white border-b shadow-sm px-4 py-2 flex flex-wrap items-center justify-start gap-4">
       <EditorToolbarFormat />
       <EditorToolbarText />
     </div>
