694d31ce95d50822999a060ac578fa99fe54503c
Blackleelou <jantzen.chris@gmail.com>
2025-06-16 17:00:28 +0200

    Update AdminPanel.tsx

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -3,9 +3,9 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import simpleRestDataProvider from 'ra-data-simple-rest';
-import authProvider from '@/lib/admin/authProvider'; // Passe Pfad ggf. an
+import authProvider from '@/lib/admin/authProvider';
 
-const dataProvider = simpleRestDataProvider('/api'); // API-Endpoint
+const dataProvider = simpleRestDataProvider('/api');
 
 const AdminPanel: React.FC = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
