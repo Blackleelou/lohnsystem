cab30e4e9832f04ce376e4eed4390fea458bf140
Blackleelou <jantzen.chris@gmail.com>
2025-06-16 18:49:24 +0200

    Update list.ts

--- src/pages/api/editor/list.ts
+++ src/pages/api/editor/list.ts
@@ -39,7 +39,7 @@ const handler: NextApiHandler = async (req, res) => {
         format: true,
         updatedAt: true,
         ownerId: true,
-        companyID: true,
+        companyId: true,
       },
     });
 
