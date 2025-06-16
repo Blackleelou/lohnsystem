8f541c08259d128d94c5ef1a915c5e11e1fb17b3
Blackleelou <jantzen.chris@gmail.com>
2025-06-16 07:26:25 +0200

    Update AdminPanel.tsx

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -1,5 +1,5 @@
 // src/components/admin/AdminPanel.tsx
-import { AdminContext, AdminUI, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import simpleRestDataProvider from 'ra-data-simple-rest';
 import authProvider from '@/lib/admin/authProvider';
 
@@ -7,12 +7,10 @@ const dataProvider = simpleRestDataProvider('/api/admin');
 
 export default function AdminPanel() {
   return (
-    <AdminContext dataProvider={dataProvider} authProvider={authProvider}>
-      <AdminUI>
-        <Resource name="users" list={ListGuesser} />
-        <Resource name="shifts" list={ListGuesser} />
-        <Resource name="payrules" list={ListGuesser} />
-      </AdminUI>
-    </AdminContext>
+    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+      <Resource name="users" list={ListGuesser} />
+      <Resource name="shifts" list={ListGuesser} />
+      <Resource name="payrules" list={ListGuesser} />
+    </Admin>
   );
 }
